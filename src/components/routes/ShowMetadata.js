7300b27c059f4cfd9acdb2f48880626472c49821
Josef Hardi <josef.hardi@gmail.com>
2022-06-07 18:09:14 -0700

    fix: fix warning message regarding pre inside Typography

--- src/components/routes/ShowMetadata.js
+++ src/components/routes/ShowMetadata.js
@@ -1,14 +1,13 @@
 import React, {useState} from "react";
-import Typography from "@mui/material/Typography";
 
 export default function ShowMetadata({metadataRecord}) {
     const [show, setShow] = useState(false);
     return (
         <>
             <button onClick={() => setShow(prev => !prev)}>Show metadata</button>
-            {show && <Typography style={{width: "85em"}}>
-                <pre>{JSON.stringify(metadataRecord, null, 3)}</pre>
-            </Typography>}
+            {show && <pre style={{fontSize: "1.1em", width: "85em"}}>
+                {JSON.stringify(metadataRecord, null, 3)}
+            </pre>}
         </>
     );
 }
