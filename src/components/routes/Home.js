d4bd5c837be2d6f074d1f289ae9477aa112f52fc
Josef Hardi <josef.hardi@gmail.com>
2022-06-23 15:32:59 -0700

    feat: added funder logos

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,41 +1,73 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import {Link} from "react-router-dom";
+import Button from "@mui/material/Button";
+import Typography from "@material-ui/core/Typography";
+import Box from "@mui/material/Box";
 import SimpleHeader from "../common/SimpleHeader";
 import AppFooter from "../common/AppFooter";
-import StyledEngineProvider from "@mui/material/StyledEngineProvider";
-import Typography from "@material-ui/core/Typography";
+import cihrLogo from '../../../src/resources/img/cihr-logo.png';
+import fwfLogo from '../../../src/resources/img/fwf-logo.png';
+import nihrLogo from '../../../src/resources/img/nihr-logo.png';
+import snsfLogo from '../../../src/resources/img/snsf-logo.png';
+import wellcomeLogo from '../../../src/resources/img/wellcome-logo.png';
 
 export default function Home() {
 
-  return (
-    <>
-      <SimpleHeader/>
-      <div id="appContent">
-        <div className="mainTitle">Making data fair</div>
-        <div className="mainSubtitle">Get suggestions for improving your metadata. Make your research data more
-          Findable,
-          Accessible, Interoperable
-          and Reusable
-        </div>
-        <div id={"mainButtons"}>
-          <Button component={Link}
-                  to="/ResearchNotPublished"
-                  className={"mainButton"}
-                  variant={"contained"}
-                  size={"large"}>
-            My research is not yet published</Button>
-          <Button
-            component={Link}
-            to="/EvaluateMetadata"
-            className={"mainButton"}
-            variant={"contained"}
-            size={"large"}>
-            My research is published</Button>
-        </div>
-        <Typography align={"center"}><Link to={"/PrivacyPolicy"}>How this tool uses your data</Link></Typography>
-      </div>
-      <AppFooter/>
-    </>
-  );
+    return (
+        <>
+            <SimpleHeader/>
+            <div id="appContent">
+                <div className="mainTitle">MAKING DATA FAIR</div>
+                <div className="mainSubtitle">Get suggestions for improving your metadata. Make your research data more
+                    Findable,
+                    Accessible, Interoperable
+                    and Reusable
+                </div>
+                <div id={"mainButtons"}>
+                    <Button
+                        component={Link}
+                        to="/EvaluateMetadata"
+                        className={"mainButton"}
+                        variant={"contained"}
+                        size={"large"}>
+                        Start Evaluating Published Data</Button>
+                </div>
+                <Typography align={"center"}><Link to={"/PrivacyPolicy"}>How this tool uses your
+                    data</Link></Typography>
+                <div style={{marginTop: "12vh"}}>
+                    <Box
+                        component="img"
+                        sx={{height: 100}}
+                        alt="Wellcome Trust logo"
+                        src={wellcomeLogo}
+                    />
+                    <Box
+                        component="img"
+                        sx={{height: 100}}
+                        alt="Canadian Institutes of Health Research logo"
+                        src={cihrLogo}
+                    />
+                    <Box
+                        component="img"
+                        sx={{height: 100}}
+                        alt="National Institute for Health and Care Research logo"
+                        src={nihrLogo}
+                    />
+                    <Box
+                        component="img"
+                        sx={{height: 100}}
+                        alt="Swiss National Science Foundation logo"
+                        src={snsfLogo}
+                    />
+                    <Box
+                        component="img"
+                        sx={{height: 100}}
+                        alt="FWF der Wissenschaftsfonds logo"
+                        src={fwfLogo}
+                    />
+                </div>
+            </div>
+            <AppFooter/>
+        </>
+    );
 }
