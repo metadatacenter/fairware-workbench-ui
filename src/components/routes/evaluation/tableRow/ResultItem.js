9f3a68031f1cb8f59b79118054c7c82dfa9868a6
Josef Hardi <josef.hardi@gmail.com>
2022-06-23 14:51:21 -0700

    feat: added icons

--- src/components/routes/evaluation/tableRow/ResultItem.js
+++ src/components/routes/evaluation/tableRow/ResultItem.js
@@ -3,6 +3,9 @@ import {useNavigate} from "react-router-dom";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell/TableCell";
 import Button from "@mui/material/Button";
+import SvgIcon from '@mui/material/SvgIcon';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import DownloadIcon from '@mui/icons-material/Download';
 
 export default function ResultItem({metadataRecord, metadataIndex, metadataEvaluationResult}) {
     const navigate = useNavigate();
@@ -29,8 +32,8 @@ export default function ResultItem({metadataRecord, metadataIndex, metadataEvalu
                 </TableCell>
                 <TableCell align="center">{metadataEvaluationResult.evaluationReport.evaluationReportItems.length}
                 </TableCell>
-                <TableCell align="center">PREVIEW ICON</TableCell>
-                <TableCell align="center">DOWNLOAD ICON</TableCell>
+                <TableCell align="center"><SvgIcon component={VisibilityIcon} inheritViewBox /></TableCell>
+                <TableCell align="center"><SvgIcon component={DownloadIcon} inheritViewBox /></TableCell>
                 <TableCell align="center">
                     <Button
                         onClick={handleStartEvaluateButtonClick}
