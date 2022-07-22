96d2724d87d11a85be5673072c9bb06b260383de
Josef Hardi <josef.hardi@gmail.com>
2022-07-22 13:03:11 -0700

    refactor: remove unused functions

--- src/util/commonUtil.js
+++ src/util/commonUtil.js
@@ -1,58 +1,7 @@
-import {isCedarUri, isCedarTemplateInstanceUri, isCedarTemplateUri} from "./cedarUtil";
-import {CEDAR_EDIT_TEMPLATE_BASE_URI, CEDAR_EDIT_TEMPLATE_INSTANCE_BASE_URI, DOI_BASE_URI} from "../constants";
-
 /**
  * Common Utilities
  */
 
 export function removeDuplicates(array) {
   return [...new Set(array)];
-};
-
-export function truncate(str, num) {
-  if (str.length <= num) {
-    return str
-  }
-  return str.slice(0, num) + '...'
-};
-
-export function shortenUrl(url, onlyEnd) {
-  if (url.length > 50) {
-    let end = url.slice(url.length - 16);
-    let start = url.substr(0, 16);
-    if (onlyEnd) {
-      return end;
-    }
-    else {
-      return start + '...' + end;
-    }
-  }
-  else {
-    return url;
-  }
-};
-
-export function generateHref(uri) {
-  console.log(uri)
-  if (isCedarTemplateUri(uri)) {
-    return CEDAR_EDIT_TEMPLATE_BASE_URI + uri;
-  }
-  else if (isCedarTemplateInstanceUri(uri)) {
-    return CEDAR_EDIT_TEMPLATE_INSTANCE_BASE_URI + uri;
-  }
-  else if (!uri.startsWith("http") && !uri.startsWith("https")) {
-    return DOI_BASE_URI + uri;
-  }
-  else {
-    return uri;
-  }
-}
-
-export function generateMetadataRecordName(uri, recordTitle) {
-  if (recordTitle) {
-    return recordTitle + ' (' + shortenUrl(uri, true) + ')';
-  }
-  else {
-    return shortenUrl(uri);
-  }
 }
